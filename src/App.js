4b89a4661d56fea7b9ef10844e53f70a79d1df57
mwizerwa77 <mwizerwa77@gmail.com>
2022-03-08 23:02:02 +0200

    done with aws deployer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there</p>
+        <p>Able to deploy on AWS Beanstalk</p>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hi there/i);
+  const linkElement = screen.getByText(/Able to deploy on AWS Beanstalk/i);
   expect(linkElement).toBeInTheDocument();
 });
